0a7195a16506035a4034aab6130527a1ca3905d5
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-02 00:24:54 -0300

    un par de gets simples

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,7 +4,18 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    Javalin app = Javalin.create().start(7070);
+    Javalin app = Javalin.create();
+
     app.get("/", ctx -> ctx.result("Hello World"));
+
+    app.get("/hello/{name}", ctx -> {
+      ctx.result("Hello " + ctx.pathParam("name").toUpperCase());
+    });
+
+    app.get("/path/*", ctx -> {
+      ctx.result("You are here because " + ctx.path() + " matches " + ctx.matchedPath());
+    });
+
+    app.start(7070);
   }
 }
