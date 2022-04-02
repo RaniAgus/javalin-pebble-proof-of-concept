413ded616afcec0c1cca6343e9c29b4dc63e29dd
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-02 11:19:53 -0300

    agrego vista de users

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,12 +2,15 @@ package org.example;
 
 import io.javalin.Javalin;
 import io.javalin.core.JavalinConfig;
+import io.javalin.plugin.rendering.vue.VueComponent;
 import org.example.dao.UserRepository;
 import org.example.dao.UserNotFoundException;
 
-public class HelloWorld {
+public class Application {
   public static void main(String[] args) {
     Javalin app = Javalin.create(JavalinConfig::enableWebjars);
+
+    app.get("/users", new VueComponent("user-overview"));
 
     app.get("/api/users", UserRepository::getAll);
     app.get("/api/users/{user-id}", UserRepository::getOne);
