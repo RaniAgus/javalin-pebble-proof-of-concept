6ecd89c2399d6cdecb6004c7b6056841ebd28b1c
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-28 23:09:00 -0300

    quito uso de Lombok

--- src/main/java/org/example/data/Post.java
+++ src/main/java/org/example/data/Post.java
@@ -1,18 +1,62 @@
 package org.example.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Post {
   private String userId;
   private String userName;
   private LocalDate date;
   private Integer likes;
   private String details;
+
+  public Post() {
+  }
+
+  public Post(String userId, String userName, LocalDate date, Integer likes, String details) {
+    this.userId = userId;
+    this.userName = userName;
+    this.date = date;
+    this.likes = likes;
+    this.details = details;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public Integer getLikes() {
+    return likes;
+  }
+
+  public void setLikes(Integer likes) {
+    this.likes = likes;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
+  }
 }
--- src/main/java/org/example/data/User.java
+++ src/main/java/org/example/data/User.java
@@ -1,12 +1,7 @@
 package org.example.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
 public class User {
   private String id;
   private String firstName;
@@ -14,4 +9,64 @@ public class User {
   private LocalDate birthday;
   private String gender;
   private String email;
+
+  public User() {
+  }
+
+  public User(String id, String firstName, String lastName, LocalDate birthday, String gender, String email) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = birthday;
+    this.gender = gender;
+    this.email = email;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public LocalDate getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(LocalDate birthday) {
+    this.birthday = birthday;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
