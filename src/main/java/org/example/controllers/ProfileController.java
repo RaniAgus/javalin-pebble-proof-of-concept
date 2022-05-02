824bfb80d05f242c8f28816c9c084d14a34bab82
Agustin Ranieri <aguseranieri@gmail.com>
2022-05-02 16:12:35 -0300

    delego validaciones a EditUserProfileForm

--- src/main/java/org/example/controllers/ProfileController.java
+++ src/main/java/org/example/controllers/ProfileController.java
@@ -1,20 +1,16 @@
 package org.example.controllers;
 
-import io.javalin.core.validation.ValidationException;
 import io.javalin.http.Context;
 import io.javalin.http.UploadedFile;
-import org.apache.commons.validator.routines.EmailValidator;
+import org.example.data.EditUserProfileForm;
 import org.example.data.User;
 import org.example.repository.UserRepository;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
-import java.time.LocalDate;
-
 import static io.javalin.core.util.FileUtil.streamToFile;
 import static io.javalin.plugin.rendering.template.TemplateUtil.model;
 import static java.time.LocalDate.now;
-import static java.util.Arrays.asList;
 
 public class ProfileController implements WithGlobalEntityManager, TransactionalOps {
   private final UserRepository users;
@@ -49,45 +45,24 @@ public class ProfileController implements WithGlobalEntityManager, Transactional
   }
 
   public void editUserProfile(Context ctx) {
-    Long userId = ctx.sessionAttribute("userId");
+    EditUserProfileForm form = new EditUserProfileForm(ctx);
+    if (!form.isValid()) {
+      ctx.status(400).redirect("/profiles/me/edit?error=true");
+      return;
+    }
+
     try {
-      User user = new User(
-          ctx.formParamAsClass("firstName", String.class).get(),
-          ctx.formParamAsClass("lastName", String.class).get(),
-          ctx.formParamAsClass("birthday", LocalDate.class)
-              .allowNullable()
-              .check(
-                  localDate -> localDate.isBefore(now().plusDays(1)),
-                  "Birthday must be today or earlier."
-              )
-              .get(),
-          ctx.formParamAsClass("gender", String.class)
-              .check(
-                  gender -> asList("Female", "Male", "Other").contains(gender),
-                  "Gender must be one of: Female, Male, Other."
-              )
-              .get(),
-          ctx.formParamAsClass("email", String.class)
-              .check(
-                  email -> EmailValidator.getInstance().isValid(email),
-                  "Invalid email address."
-              )
-              .get()
-      );
-      user.setId(userId);
-      UploadedFile photo = ctx.uploadedFile("photo");
+      User user = form.updateUser(users.getById(form.getId()));
+      UploadedFile photo = form.getPhoto();
 
       entityManager().getTransaction().begin();
       this.users.update(user);
-      if (photo != null && photo.getSize() > 0) {
-        streamToFile(photo.getContent(), "static/images/" + userId + ".jpg");
+      if (photo != null) {
+        streamToFile(photo.getContent(), "static/images/" + user.getId() + ".jpg");
       }
       entityManager().getTransaction().commit();
 
       ctx.redirect("/profiles/me");
-
-    } catch (ValidationException e) {
-      ctx.status(400).redirect("/profiles/me/edit?error=true");
     } catch (Exception e) {
       ctx.status(500).redirect("/profiles/me/edit?error=true");
     }
---
+++ src/main/java/org/example/data/EditUserProfileForm.java
@@ -0,0 +1,94 @@
+package org.example.data;
+
+import io.javalin.core.validation.*;
+import io.javalin.http.Context;
+import io.javalin.http.UploadedFile;
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.time.LocalDate.now;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
+public class EditUserProfileForm {
+  private final Validator<Long> id;
+  private final Validator<String> firstName;
+  private final Validator<String> lastName;
+  private final NullableValidator<LocalDate> birthday;
+  private final Validator<String> gender;
+  private final Validator<String> email;
+  private final UploadedFile photo;
+  private final Map<String, List<String>> errors;
+
+  public EditUserProfileForm(Context ctx) {
+    this.id = ctx.formParamAsClass("id", Long.class)
+        .check(
+            id -> id == ctx.sessionAttribute("userId"),
+            "Invalid user ID"
+        );
+    this.firstName = ctx.formParamAsClass("firstName", String.class);
+    this.lastName = ctx.formParamAsClass("lastName", String.class);
+    this.birthday = ctx.formParamAsClass("birthday", LocalDate.class)
+        .allowNullable()
+        .check(
+            localDate -> localDate.isBefore(now().plusDays(1)),
+            "Birthday must be today or earlier."
+        );
+    this.gender = ctx.formParamAsClass("gender", String.class)
+        .check(
+            gender -> asList("Female", "Male", "Other").contains(gender),
+            "Gender must be one of: Female, Male, Other."
+        );
+    this.email = ctx.formParamAsClass("email", String.class)
+        .check(
+            email -> EmailValidator.getInstance().isValid(email),
+            "Invalid email address."
+        );
+    this.photo = ctx.uploadedFile("photo");
+    this.errors = collectErrors();
+  }
+
+  public boolean isValid() {
+    return this.errors.isEmpty();
+  }
+
+  public Long getId() {
+    return id.get();
+  }
+
+  public User updateUser(User user) {
+    user.setFirstName(firstName.get());
+    user.setLastName(lastName.get());
+    user.setBirthday(birthday.get());
+    user.setGender(gender.get());
+    user.setEmail(email.get());
+
+    return user;
+  }
+
+  public UploadedFile getPhoto() {
+    return photo != null && photo.getSize() > 0 ? photo : null;
+  }
+
+  private Map<String, List<String>> collectErrors() {
+    Map<String, List<String>> errors = Stream.of(id, firstName, lastName, birthday, gender, email)
+        .flatMap(v -> v.errors().entrySet().stream())
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            m -> m.getValue().stream().map(ValidationError::getMessage).collect(Collectors.toList())
+        ));
+
+    UploadedFile photo = getPhoto();
+    String contentType = photo != null ? photo.getContentType() : null;
+    if (photo != null && (contentType == null || !contentType.equals("image/jpeg"))) {
+      errors.put("photo", singletonList("Photo must be in jpeg format"));
+    }
+
+    return errors;
+  }
+}
